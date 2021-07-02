d546cc4f4334cde82def4f0341b94765b0252599
TechieGuy12 <psalmon12@gmail.com>
2021-07-01 20:46:17 -0400

    Added command-line parameters

--- FileWatcher/Program.cs
+++ FileWatcher/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.CommandLine;
+using System.CommandLine.Invocation;
 using System.IO;
+using System.Reflection;
 using System.Threading.Tasks;
 using System.Xml.Serialization;
 using TE.FileWatcher.Configuration;
@@ -15,8 +18,15 @@ namespace TE.FileWatcher
     {
         // Success return code
         private const int SUCCESS = 0;
+
         // Error return code
         private const int ERROR = -1;
+
+        // The default configuration file name
+        const string DEFAULT_CONFIG_FILE = "config.xml";
+
+        // The default notifications file name
+        const string DEFAULT_NOTIFICATION_FILE = "notification.xml";
 
         /// <summary>
         /// The main function.
@@ -28,16 +38,70 @@ namespace TE.FileWatcher
         /// Returns 0 on success, otherwise non-zero.
         /// </returns>
         static int Main(string[] args)
-        { 
+        {
+            RootCommand rootCommand = new RootCommand(
+                description: "Monitors files and folders for changes.");
+
+            Option<string> configFolder = new Option<string>(
+                aliases: new string[] { "--folder", "-f" },
+                description: "The folder containing the configuration and notification XML files.");
+            rootCommand.AddOption(configFolder);
+
+            Option<string> configName = new Option<string>(
+                aliases: new string[] { "--configFile", "-cf" },
+                description: "The name of the configuration XML file.");
+            rootCommand.AddOption(configName);
+
+            Option<string> notificationName = new Option<string>(
+                aliases: new string[] { "--notificationFile", "-nf" },
+                description: "The name of the notification XML file.");
+            rootCommand.AddOption(notificationName);
+
+            rootCommand.Handler = CommandHandler.Create<string, string, string>(RunWatcher);
+            return rootCommand.Invoke(args);
+        }
+
+        /// <summary>
+        /// Runs the file/folder watcher.
+        /// </summary>
+        /// <param name="folder">
+        /// The folder where the config and notifications files are stored.
+        /// </param>
+        /// <param name="configFileName">
+        /// The name of the configuration file.
+        /// </param>
+        /// <param name="notificationsFileName">
+        /// The name of the notifications file.
+        /// </param>
+        /// <returns>
+        /// Returns 0 if no error occurred, otherwise non-zero.
+        /// </returns>
+        private static int RunWatcher(string folder, string configFileName, string notificationsFileName)
+        {
+            Console.WriteLine($"File: {notificationsFileName}.");
+            // Get the config file path
+            string configFile = GetConfigFilePath(folder, configFileName);
+            if (string.IsNullOrWhiteSpace(configFile))
+            {
+                return ERROR;
+            }
+
+            // Get the notifications file path
+            string notificationsFile = GetNotificationsFilePath(folder, notificationsFileName);
+            if (string.IsNullOrWhiteSpace(notificationsFile))
+            {
+                return ERROR;
+            }
+
             // Load the watches information from the config XML file
-            Watches watches = ReadConfigFile(@"C:\Temp\config.xml");
+            Watches watches = ReadConfigFile(configFile);
             if (watches == null)
             {
                 return ERROR;
             }
 
             // Load the notifications from the XML file
-            Notifications.Notifications notifications = ReadNotificationFile(@"C:\Temp\notification.xml");
+            Notifications.Notifications notifications = ReadNotificationFile(notificationsFile);
             if (notifications == null)
             {
                 return ERROR;
@@ -222,6 +286,133 @@ namespace TE.FileWatcher
             }
 
             return true;
+        }
+
+        /// <summary>
+        /// Gets the folder path containing the configuration and notification
+        /// files.
+        /// </summary>
+        /// <param name="path">
+        /// The folder path.
+        /// </param>
+        /// <returns>
+        /// The folder path of the files, otherwise null.
+        /// </returns>
+        private static string GetFolderPath(string path)
+        {
+            if (string.IsNullOrWhiteSpace(path))
+            {
+                try
+                {
+                    path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"The folder name is null or empty. Couldn't get the current location. Reason: {ex.Message}");
+                    return null;
+                }
+            }
+
+            if (Directory.Exists(path))
+            {
+                return path;
+            }
+            else
+            {
+                Console.WriteLine("The folder does not exist.");
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Gets the full path to the configuration file.
+        /// </summary>
+        /// <param name="path">
+        /// The path to the configuration file.
+        /// </param>
+        /// <param name="name">
+        /// The name of the configuration file.
+        /// </param>
+        /// <returns>
+        /// The full path to the configuration file, otherwise null.
+        /// </returns>
+        private static string GetConfigFilePath(string path, string name)
+        {
+            string folderPath = GetFolderPath(path);
+            if (folderPath == null)
+            {
+                return null;
+            }
+
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                name = DEFAULT_CONFIG_FILE;
+            }
+
+            try
+            {
+                string fullPath = Path.Combine(folderPath, name);
+                if (File.Exists(fullPath))
+                {
+                    return fullPath;
+                }
+                else
+                {
+                    Console.WriteLine($"The configuration file '{fullPath}' was not found.");
+                    return null;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Could not get the path to the configuration file. Reason: {ex.Message}");
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Gets the full path to the notifications file.
+        /// </summary>
+        /// <param name="path">
+        /// The path to the notifications file.
+        /// </param>
+        /// <param name="name">
+        /// The name of the notifications file.
+        /// </param>
+        /// <returns>
+        /// The full path to the notifications file, otherwise null.
+        /// </returns>
+        private static string GetNotificationsFilePath(string path, string name)
+        {
+            string folderPath = GetFolderPath(path);
+            if (folderPath == null)
+            {
+                return null;
+            }
+
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                name = DEFAULT_NOTIFICATION_FILE;
+            }
+
+            try
+            {
+                string fullPath = Path.Combine(folderPath, name);
+                if (File.Exists(fullPath))
+                {
+                    Console.WriteLine($"Notifications file: {fullPath}.");
+                    return fullPath;
+                }
+                else
+                {
+                    Console.WriteLine($"The notifications file '{fullPath}' was not found.");
+                    return null;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Could not get the path to the notifications file. Reason: {ex.Message}");
+                return null;
+            }
         }
     }
 }
