26db58de7cd9e2be1f53e847e3e6303fef37a8e8
TechieGuy12 <psalmon12@gmail.com>
2021-07-05 17:07:29 -0400

    Added the Actions classes

---
+++ FileWatcher/Configuration/Actions/Action.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using TE.FileWatcher.Logging;
+
+namespace TE.FileWatcher.Configuration.Actions
+{
+    public class Action
+    {
+        /// <summary>
+        /// The type of action to perform.
+        /// </summary>
+        [Serializable]
+        public enum ActionType
+        {
+            /// <summary>
+            /// Copy a file.
+            /// </summary>
+            Copy,
+            /// <summary>
+            /// Move a file.
+            /// </summary>
+            Move,
+            /// <summary>
+            /// Delete a file.
+            /// </summary>
+            Delete
+        }
+
+        /// <summary>
+        /// Gets or sets the type of action to perform.
+        /// </summary>
+        [XmlElement("type")]
+        public ActionType Type { get; set; }
+
+        /// <summary>
+        /// Gets or sets the source of the action.
+        /// </summary>
+        [XmlElement("source")]
+        public string Source { get; set; }
+
+        /// <summary>
+        /// Gets or sets the destination of the action.
+        /// </summary>
+        [XmlElement("destination")]
+        public string Destination { get; set; }
+
+        /// <summary>
+        /// Gets or sets the verify flag.
+        /// </summary>
+        [XmlElement(ElementName = "verify", DataType = "boolean")]
+        public bool Verify { get; set; }
+    }
+}
---
+++ FileWatcher/Configuration/Actions/Actions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using TE.FileWatcher.Logging;
+
+namespace TE.FileWatcher.Configuration.Actions
+{
+    [XmlRoot("actions")]
+    public class Actions
+    {
+        /// <summary>
+        /// Gets or sets the list of actions to perform.
+        /// </summary>
+        [XmlElement("action")]
+        public List<Action> ActionList { get; set; } = new List<Action>();
+    }
+}
