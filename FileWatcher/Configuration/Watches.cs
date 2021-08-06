eac36c394ba11b62b4c82a670078b61df60ee77a
TechieGuy12 <psalmon12@gmail.com>
2021-08-06 14:24:17 -0400

    Added Start() method

--- FileWatcher/Configuration/Watches.cs
+++ FileWatcher/Configuration/Watches.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Serialization;
+using TE.FileWatcher.Logging;
 
 namespace TE.FileWatcher.Configuration
 {
@@ -24,5 +25,28 @@ namespace TE.FileWatcher.Configuration
         /// </summary>
         [XmlElement("watch")]
         public List<Watch> WatchList { get; set; }
+
+        [XmlIgnore]
+        List<Watcher> _runningWatchers;
+
+        /// <summary>
+        /// Starts the watches.
+        /// </summary>
+        public void Start()
+        {
+            _runningWatchers = new List<Watcher>();
+
+            foreach (Watch watch in WatchList)
+            {
+                try
+                {
+                    _runningWatchers.Add(new Watcher(watch));
+                }
+                catch (Exception ex)
+                {
+                    Logger.WriteLine(ex.Message, LogLevel.ERROR);
+                }
+            }
+        }
     }
 }
