6088b624fa38beba176ff31def959be379abc9f4
TechieGuy12 <psalmon12@gmail.com>
2021-08-07 21:41:17 -0400

    Abstracted config file functionality

--- FileWatcher/Program.cs
+++ FileWatcher/Program.cs
@@ -1,13 +1,8 @@
 ﻿using System;
 using System.CommandLine;
 using System.CommandLine.Invocation;
-using System.IO;
-using System.Reflection;
-using System.Threading.Tasks;
-using System.Xml.Serialization;
 using TE.FileWatcher.Configuration;
 using TE.FileWatcher.Logging;
-using TE.FileWatcher.Configuration.Notifications;
 
 namespace TE.FileWatcher
 {
@@ -21,9 +16,6 @@ namespace TE.FileWatcher
 
         // Error return code
         private const int ERROR = -1;
-
-        // The default configuration file name
-        const string DEFAULT_CONFIG_FILE = "config.xml";
 
         /// <summary>
         /// The main function.
@@ -47,7 +39,7 @@ namespace TE.FileWatcher
                     description: "The name of the configuration XML file."),
             };
             rootCommand.Description = "Monitors files and folders for changes.";
-            rootCommand.Handler = CommandHandler.Create<string, string>(RunWatcher);
+            rootCommand.Handler = CommandHandler.Create<string, string>(Run);
 
             return rootCommand.Invoke(args);
         }
@@ -64,17 +56,12 @@ namespace TE.FileWatcher
         /// <returns>
         /// Returns 0 if no error occurred, otherwise non-zero.
         /// </returns>
-        private static int RunWatcher(string folder, string configFile)
-        {
-            // Get the config file path
-            string configFilePath = GetConfigFilePath(folder, configFile);
-            if (string.IsNullOrWhiteSpace(configFilePath))
-            {
-                return ERROR;
-            }
+        private static int Run(string folder, string configFile)
+        {            
+            IConfigurationFile config = new XmlFile(folder, configFile);
 
             // Load the watches information from the config XML file
-            Watches watches = ReadConfigFile(configFilePath);
+            Watches watches = config.Read();
             if (watches == null)
             {
                 return ERROR;
@@ -94,42 +81,6 @@ namespace TE.FileWatcher
             else
             {
                 return ERROR;
-            }
-        }
-
-        /// <summary>
-        /// Reads the configuration XML file.
-        /// </summary>
-        /// <param name="path">
-        /// The path to the configuration XML file.
-        /// </param>
-        /// <returns>
-        /// A watches object if the file was read successfully, otherwise null.
-        /// </returns>
-        private static Watches ReadConfigFile(string path)
-        {
-            if (string.IsNullOrWhiteSpace(path))
-            {
-                Console.WriteLine("The configuration file path was null or empty.");
-                return null;
-            }
-
-            if (!File.Exists(path))
-            {
-                Console.WriteLine($"The configuration file path '{path}' does not exist.");
-                return null;
-            }
-
-            try
-            {
-                XmlSerializer serializer = new XmlSerializer(typeof(Watches));
-                using FileStream fs = new FileStream(path, FileMode.Open);
-                return (Watches)serializer.Deserialize(fs);
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"The configuration file could not be read. Reason: {ex.Message}");
-                return null;
             }
         }
 
@@ -184,88 +135,6 @@ namespace TE.FileWatcher
 
             Logger.WriteLine("All watchers have closed.");
             return true;
-        }
-
-        /// <summary>
-        /// Gets the folder path containing the configuration and notification
-        /// files.
-        /// </summary>
-        /// <param name="path">
-        /// The folder path.
-        /// </param>
-        /// <returns>
-        /// The folder path of the files, otherwise null.
-        /// </returns>
-        private static string GetFolderPath(string path)
-        {
-            if (string.IsNullOrWhiteSpace(path))
-            {
-                try
-                {
-                    path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine($"The folder name is null or empty. Couldn't get the current location. Reason: {ex.Message}");
-                    return null;
-                }
-            }
-
-            if (Directory.Exists(path))
-            {
-                return path;
-            }
-            else
-            {
-                Console.WriteLine("The folder does not exist.");
-                return null;
-            }
-        }
-
-        /// <summary>
-        /// Gets the full path to the configuration file.
-        /// </summary>
-        /// <param name="path">
-        /// The path to the configuration file.
-        /// </param>
-        /// <param name="name">
-        /// The name of the configuration file.
-        /// </param>
-        /// <returns>
-        /// The full path to the configuration file, otherwise null.
-        /// </returns>
-        private static string GetConfigFilePath(string path, string name)
-        {
-            string folderPath = GetFolderPath(path);
-            if (folderPath == null)
-            {
-                return null;
-            }
-
-            if (string.IsNullOrWhiteSpace(name))
-            {
-                name = DEFAULT_CONFIG_FILE;
-            }
-
-            try
-            {
-                string fullPath = Path.Combine(folderPath, name);
-                if (File.Exists(fullPath))
-                {
-                    Console.WriteLine($"Configuration file: {fullPath}.");
-                    return fullPath;
-                }
-                else
-                {
-                    Console.WriteLine($"The configuration file '{fullPath}' was not found.");
-                    return null;
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Could not get the path to the configuration file. Reason: {ex.Message}");
-                return null;
-            }
         }
     }
 }
