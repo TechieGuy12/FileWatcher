4c09015a0b47882a1bf02027047a9318b14bfe27
TechieGuy12 <psalmon12@gmail.com>
2024-06-21 17:32:00 -0400

    Added the variables-related elements classes

---
+++ src/Configuration/Variable.cs
@@ -0,0 +1,13 @@
+﻿using System.Xml.Serialization;
+
+namespace TE.FileWatcher.Configuration
+{
+    public class Variable
+    {
+        [XmlElement("name")]
+        public string Name { get; set; }
+
+        [XmlElement("value")]
+        public string Value { get; set; }
+    }
+}
---
+++ src/Configuration/Variables.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.ObjectModel;
+using System.Xml.Serialization;
+
+namespace TE.FileWatcher.Configuration
+{
+    /// <summary>
+    /// Contains information about all variables.
+    /// </summary>
+    [XmlRoot("variables")]
+    public class Variables
+    {
+        /// <summary>
+        /// Gets or sets the list of variables.
+        /// </summary>
+        [XmlElement("variable")]
+        public Collection<Action>? VariableList { get; set; }
+    }
+}
