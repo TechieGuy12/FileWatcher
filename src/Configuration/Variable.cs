885665405d05d4c7bd98247cc7fb9a106cd25fbd
TechieGuy12 <psalmon12@gmail.com>
2024-07-05 11:10:59 -0400

    Changed the variable name and value to nullable

--- src/Configuration/Variable.cs
+++ src/Configuration/Variable.cs
@@ -4,10 +4,16 @@ namespace TE.FileWatcher.Configuration
 {
     public class Variable
     {
+        /// <summary>
+        /// Gets or sets the name of the variable.
+        /// </summary>
         [XmlElement("name")]
-        public string Name { get; set; }
+        public string? Name { get; set; }
 
+        /// <summary>
+        /// Gets or sets the name of the variable.
+        /// </summary>
         [XmlElement("value")]
-        public string Value { get; set; }
+        public string? Value { get; set; }
     }
 }
